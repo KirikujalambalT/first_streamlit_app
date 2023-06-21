463f8f2dbaca138c37c335faa0af51910be54839
KirikujalambalT <133020715+KirikujalambalT@users.noreply.github.com>
2023-06-21 12:26:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My New Coding Challenge')
+streamlit.heading('My skills')
+streamlit.body('Python
+               SQL
+               Tableau
+               Power BI
+               Snowflake')
